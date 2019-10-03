507b4bf4ffec9cf5c8eb1e0da4bead8a62770e87
dnguyen19 <dnguyen145@my.bcit.ca>
2019-10-03 10:18:27 -0700

    added denise in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,12 @@ namespace GitHubLab.Controllers
             return View();
         }
 
+        public IActionResult Denis()
+        {
+            ViewData["Message"] = "denise.";
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
